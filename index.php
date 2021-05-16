294f7b178f4ae67093870d88da112c7ca7211478
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-05-16 00:26:04 -0400

    Define a survey route that displays a short survey

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ session_start();
 
 //Require necessary files
 require_once ('vendor/autoload.php');
+require_once ('model/data-layer.php');
+require_once ('model/validation.php');
 
 
 //Instantiate Fat-Free
@@ -28,6 +30,16 @@ $f3->route('GET /', function(){
     echo $view->render('views/home.html');
 });
 
+//Define survey route
+$f3->route('GET|POST /midSurvey', function($f3){
+
+    //Get the questions from the Model and send them to the View
+    $f3->set('questions', getQuestions());
+
+    //Display the survey form
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
 
 //Run Fat-Free
 $f3->run();
---
+++ model/data-layer.php
@@ -0,0 +1,10 @@
+<?php
+
+function getQuestions(){
+    return array(
+        "Hello World",
+        "Great Survey",
+        "Beautiful weather",
+        "I am home and working on midterm");
+
+}
